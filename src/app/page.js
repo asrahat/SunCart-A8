08ecd62aa189e4133fec649fc43d7239330bd08d
asrahat <asrahat4742@gmail.com>
2026-05-01 17:22:40 +0600

    trying to fixed vercel error.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import HeroSection from "@/components/homepages/HeroSection ";
 import PopularProducts from "@/components/homepages/PopularProducts";
 import SummerCareTips from "@/components/homepages/SummerCareTips ";
 import TopBrands from "@/components/homepages/TopBrands ";
-import Image from "next/image";
 
 export default function Home() {
   return (
